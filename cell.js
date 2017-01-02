6b50c2f0902b4eac2d6aad503d1727c426e5ab38
Jakub Pustelnik <jhirtiganto@gmail.com>
2017-01-02 19:29:01 +0100

    Renamed circle.js to cell.js etc.

---
+++ cell.js
@@ -0,0 +1,17 @@
+function Cell(loc, d) {
+  this.location = createCanvas(width / 2,height / 2) || loc
+  this.diameter = height || d
+
+  this.color = {
+    r: random(50,225),
+    g: random(50,225),
+    b: random(50,225),
+    a: 255
+  }
+
+  this.render = function () {
+    noStroke()
+    fill(this.color.r,this.color.g,this.color.b,this.color.a)
+    ellipse(this.location.x,this.location.y,this.diameter,this.diameter)
+  }
+}
