98b13d6a27fea6ec0820d75638b82022e89affe8
Jakub Pustelnik <jhirtiganto@gmail.com>
2017-01-02 19:40:54 +0100

    Fixed that line where i wrote createCanvas instead of createVector and changed some values

--- cell.js
+++ cell.js
@@ -1,17 +1,17 @@
 function Cell(loc, d) {
-  this.location = createCanvas(width / 2,height / 2) || loc
+  this.location = createVector(width/2,height/2) || loc
   this.diameter = height || d
 
   this.color = {
-    r: random(50,225),
-    g: random(50,225),
-    b: random(50,225),
+    r: random(50, 230),
+    g: random(50, 230),
+    b: random(50, 230),
     a: 255
   }
 
-  this.render = function () {
+  this.render = function() {
     noStroke()
-    fill(this.color.r,this.color.g,this.color.b,this.color.a)
+    fill (this.color.r, this.color.g, this.color.b, this.color.a)
     ellipse(this.location.x,this.location.y,this.diameter,this.diameter)
   }
 }
