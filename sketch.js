bb0ee49354fd8e2084ab012b72e922418e538377
Jakub Pustelnik <jhirtiganto@gmail.com>
2017-01-03 17:38:46 +0100

    Added mousePressed function in which i check which cell was clicked, and rerender function which rerenders all the cells

--- sketch.js
+++ sketch.js
@@ -2,8 +2,26 @@ var cells = []
 
 function setup() {
   createCanvas(windowWidth,windowHeight)
-  background(51)
 
   cells.push(new Cell())
-  cells[0].render()
+  rerender()
+}
+
+function mousePressed() {
+  mousePosition = createVector(mouseX,mouseY)
+
+  for (var i = 0; i < cells.length; i++) {
+    if (p5.Vector.dist(cells[i].location, mousePosition) <= cells[i].diameter / 2) {
+      console.log('pressed')
+      //rerender()
+    }
+  }
+}
+
+function rerender() {
+  background(51)
+
+  for (var i = 0; i < cells.length; i++) {
+    cells[i].render()
+  }
 }
