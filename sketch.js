7c8ef511df9e4e67f16f5d9a1953065fa2fcf917
Jakub Pustelnik <jhirtiganto@gmail.com>
2017-01-03 19:27:24 +0100

    Added code to splitCell function

--- sketch.js
+++ sketch.js
@@ -15,6 +15,7 @@ function mousePressed() {
       console.log('pressed')
       splitCell(i)
       rerender()
+      break
     }
   }
 }
@@ -28,8 +29,34 @@ function rerender() {
 }
 
 function splitCell(index) {
-  // so this is just a rough sketch of a function
+  var baseLocation = cells[index].location.copy()
+  var newDiameter = cells[index].diameter / 2
+  var offset = cells[index].diameter / 4
 
   cells.splice(index, 1)
-  cells.push(new Cell())
+
+  var newLocations = []
+  for (var i = 0; i < 4; i++){
+    newLocations.push(baseLocation.copy())
+  }
+
+  // left up
+  newLocations[0].x -= offset
+  newLocations[0].y -= offset
+
+  // right up
+  newLocations[1].x += offset
+  newLocations[1].y -= offset
+
+  // right down
+  newLocations[2].x += offset
+  newLocations[2].y += offset
+
+  // left down
+  newLocations[3].x -= offset
+  newLocations[3].y += offset
+
+  for (var i = 0; i < newLocations.length; i++){
+    cells.push(new Cell(newLocations[i], newDiameter))
+  }
 }
